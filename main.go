8152ce0caced2d4e0664bf23b6ce63a9094d452f
Alexander <effaceurs90@gmail.com>
2023-03-26 23:34:45 +0300

    first release

--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
-
+	"os/exec"
 	"os/user"
 	"path/filepath"
+	"runtime"
+	"strings"
+	"text/tabwriter"
+	"time"
 
 	"github.com/tidwall/buntdb"
 )
@@ -30,6 +33,33 @@ func add() Password {
 	return cm
 }
 
+func delete() string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Resourse to delete: ")
+	resource, _ := reader.ReadString('\n')
+	return strings.TrimSpace(resource)
+}
+
+func clearScreen() {
+	if runtime.GOOS == "windows" {
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	} else {
+		fmt.Print("\033[H\033[2J")
+	}
+}
+func help() {
+	// Display the menu
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', tabwriter.TabIndent)
+	defer w.Flush()
+	fmt.Fprintf(w, "%s\t%s\n", "add", "to add password")
+	fmt.Fprintf(w, "%s\t%s\n", "list", "to list all records")
+	fmt.Fprintf(w, "%s\t%s\n", "delete", "to delete a record")
+	fmt.Fprintf(w, "%s\t%s\n", "exit", "to exit a programm")
+	w.Flush()
+}
+
 func main() {
 
 	// get current user in ordr to store a db file in $HOME
@@ -39,19 +69,36 @@ func main() {
 		panic(err)
 	}
 
-	// Open the database
+	// Check and open the database
+	dbPath := filepath.Join(u.HomeDir, "pass.db")
+	_, err = os.Stat(filepath.Join(u.HomeDir, "pass.db"))
+	var db *buntdb.DB
+	if os.IsNotExist(err) {
+		fmt.Println("DB does not exist, creating")
+		db, err = buntdb.Open(dbPath)
+		if err != nil {
+			fmt.Println("Cannot create database")
+			panic(err)
+		} else {
+			fmt.Printf("DB has been created successfully at path %s\n", filepath.Join(u.HomeDir, "pass.db"))
+			time.Sleep(2500 * time.Millisecond)
+			clearScreen()
+		}
+	} else {
+		db, err = buntdb.Open(dbPath)
+		if err != nil {
+			fmt.Println("Cannot open database")
+			panic(err)
+		}
+	}
 
-	db, err := buntdb.Open(filepath.Join(u.HomeDir, "pass.db"))
 	reader := bufio.NewReader(os.Stdin)
 
 	// close DB after the funtion exits
 
+	help()
 	defer db.Close()
-	// Display the menu
-	fmt.Println("Options:")
-	fmt.Println("add - to add password")
-	fmt.Println("list - to list all records")
-	fmt.Println("exit - to exit a programm")
+
 	for {
 
 		// MENU
@@ -69,16 +116,27 @@ func main() {
 			}); err != nil {
 				panic(err)
 			}
-			fmt.Println("Password has been stored")
-			return
+			fmt.Println("The record has been created successfully")
 		case "delete":
-			fmt.Println("delete")
+			resource := delete()
+			err := db.Update(func(tx *buntdb.Tx) error {
+				_, err := tx.Delete(resource)
+				return err
+			})
+			if err != nil {
+				fmt.Println("The record has not been found")
+			} else {
+				fmt.Println("The record has been deleted")
+			}
 		case "list":
 			// Read all values from the database
 			err = db.View(func(tx *buntdb.Tx) error {
+				w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', tabwriter.TabIndent)
+				defer w.Flush()
+				fmt.Fprintf(w, "%s\t%s\t%s\n", "RESOURCE", "LOGIN", "PASSWORD")
 				err := tx.Ascend("", func(key, value string) bool {
 					username, password := strings.Split(value, "|")[0], strings.Split(value, "|")[1]
-					fmt.Printf("%s = %s: %s\n", key, username, password)
+					fmt.Fprintf(w, "%s\t%s\t%s\n", key, username, password)
 					return true
 				})
 				return err
@@ -87,12 +145,11 @@ func main() {
 				panic(err)
 			}
 			continue
-		case "help":
-			fmt.Println("help")
 		case "exit":
 			os.Exit(0)
 		default:
 			fmt.Println("no such command")
+			help()
 		}
 	}
 }
